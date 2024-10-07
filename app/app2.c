2127735b7efa98518d6ea17050610d2aa057d382
ywx2012 <ywx120702@tuta.io>
2024-10-07 23:23:05 +0800

    remove app/libc/type.h

--- app/app2.c
+++ app/app2.c
@@ -1,7 +1,7 @@
 
 // Copyright (c) 2023 Wang Baisheng <baisheng_wang@163.com>, Wang Shenghan. All Rights Reserved.
 
-#include "app/libc/type.h"
+#include <stdint.h>
 #include "app/libc/print.h"
 #include "app/libc/std.h"
 #include "app/libdraw/draw.h"
