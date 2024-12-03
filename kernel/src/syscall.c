5aea7eee638454dac3511b8b5ce6d50688f00a4e
ywx2012 <ywx120702@tuta.io>
2024-12-03 20:30:38 +0800

    mov syscall handler to inline assembly

--- kernel/src/syscall.c
+++ kernel/src/syscall.c
@@ -7,6 +7,7 @@
 #include <segment.h>
 #include <sched.h>
 #include <vesa.h>
+#include <tss.h>
 
 #define RF_IF 0x00000200
 
@@ -19,6 +20,24 @@ unsigned long do_fbmap();
 unsigned long do_get_mode_info(struct mode_info *mode_info);
 
 fn_ptr syscall_table[] = { do_sleep, do_shm, do_fbmap, do_get_mode_info };
+
+__attribute__((naked))
+void
+system_call() {
+  uintptr_t number;
+  __asm__("" : "=a"(number));
+  __asm__("movq %%rsp, (tss+20)(%%rip)\n"
+          "movq (tss+4)(%%rip), %%rsp\n"
+          "pushq (tss+20)(%%rip)\n"
+          "pushq %%rcx\n"
+          "pushq %%r11\n"
+          "call *%0\n"
+          "popq %%r11\n"
+          "popq %%rcx\n"
+          "popq %%rsp\n"
+          "sysretq"
+          : : "r"(syscall_table[number]));
+}
 
 void syscall_init() {
   uint64_t star_val = ((uint64_t)USER32_CS) << 48  | ((uint64_t)KERNEL_CS) << 32;
