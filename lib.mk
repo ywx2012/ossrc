5073f6e1ca35e9a99da754a10f383589ab72e546
ywx2012 <ywx120702@tuta.io>
2024-12-04 18:37:47 +0800

    fix .d generation

--- lib.mk
+++ lib.mk
@@ -8,13 +8,13 @@ $(BUILDDIR)/$(DIR)/$(DIR).a: $($(DIR)_OBJS) | makedirs
 	$(V)ar crs $@ $^
 
 $(BUILDDIR)/$(DIR)/%.o: $(DIR)/src/%.S | makedirs
-	$(V)gcc -c -o $@ $^
+	$(V)gcc -c -o $@ $<
 
 $(BUILDDIR)/$(DIR)/%.o: $(DIR)/src/%.c | makedirs
-	$(V)gcc -c $(USER_CFLAGS) -o $@ $^
+	$(V)gcc -c $(USER_CFLAGS) -o $@ $<
 
 $(BUILDDIR)/$(DIR)/%.d: $(DIR)/src/%.S | makedirs
-	$(V)gcc -MM -c -o $@ $^
+	$(V)gcc -MM -MT ${@:%.d=%.o} -c -o $@ $<
 
 $(BUILDDIR)/$(DIR)/%.d: $(DIR)/src/%.c | makedirs
-	$(V)gcc -MM -c $(USER_CFLAGS) -o $@ $^
+	$(V)gcc -MM -MT ${@:%.d=%.o} -c $(USER_CFLAGS) -o $@ $<
